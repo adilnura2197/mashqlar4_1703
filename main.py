9e257d37e779b82c982d4ba94cc01d435d5a0c46
𝕯𝖎𝖑𝖓𝖚𝖗𝖆 <adilnura2197@gmail.com>
2026-03-16 16:16:45 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,38 @@
+#1
+a = [1, 2, 3]
+print(a)
+
+a.append(4)
+print(a)
+
+
+#2
+a = [10, 20, 40]
+print(a)
+
+a.insert(2, 30)
+print(a)
+
+
+#3
+a = [5, 6, 7, 8]
+print(a)
+
+a.remove(7)
+print(a)
+
+
+#4
+a = [11, 22, 33]
+print(a)
+
+a.pop()
+print(a)
+
+
+#5
+a = [3, 6, 9, 12]
+print(a)
+
+a.pop(1)
+print(a)
